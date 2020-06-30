8c00608b25f101d6db0d68efd4c27116da1e8485
batuhangugeler <65133848+batuhangugeler@users.noreply.github.com>
2020-06-30 09:45:48 +0200

    4th commit

---
+++ data.js
@@ -0,0 +1,80 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-frame_00000_finalcolor",
+      "name": "Frame_00000_FinalColor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.004336959831229947,
+          "pitch": 0.6184877923841423,
+          "rotation": 9.42477796076938,
+          "target": "1-frame_00100_finalcolor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-frame_00100_finalcolor",
+      "name": "Frame_00100_FinalColor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": -2.14267043019829,
+        "pitch": -0.14748140618704753,
+        "fov": 1.5211197337810989
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};

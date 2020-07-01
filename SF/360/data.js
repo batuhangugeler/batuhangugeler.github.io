a2e815a8f97fbf4f50c5bc79683fa02bf4a20047
batuhangugeler <65133848+batuhangugeler@users.noreply.github.com>
2020-07-01 15:13:07 +0200

    07

---
+++ SF/360/data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-frame_00000_finalcolor",
+      "name": "Frame_00000_FinalColor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.015716916275415826,
+          "pitch": 0.12820608108331122,
+          "title": "BILD",
+          "text": <img source src='images/img01.jpg'></img>
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};

a9bd46b50b36d69580b7f343b1349fcbc04b80a4
batuhangugeler <65133848+batuhangugeler@users.noreply.github.com>
2020-12-22 08:15:57 +0100

    0

---
+++ ECO/360/data.js
@@ -0,0 +1,72 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-paulinenbrcke",
+      "name": "Paulinenbrücke",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.07146179499301653,
+          "pitch": 0.11722286513836977,
+          "rotation": 0,
+          "target": "1-sterreichischer-platz"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-sterreichischer-platz",
+      "name": "Österreichischer Platz",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};

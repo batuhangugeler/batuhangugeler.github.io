7c6bab4ea1c7b9b88b37083ce1c82fe4ec7e23c6
batuhangugeler <65133848+batuhangugeler@users.noreply.github.com>
2021-02-20 07:58:28 +0100

    065

--- ECO/360/data.js
+++ ECO/360/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-paulinenbrcke",
-      "name": "Paulinenbrücke",
+      "id": "0-perspektive-sw",
+      "name": "PERSPEKTIVE SW",
       "levels": [
         {
           "tileSize": 256,
@@ -26,17 +26,187 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.07146179499301653,
-          "pitch": 0.11722286513836977,
+          "yaw": 0.2805312140589127,
+          "pitch": 0.03131227295510186,
           "rotation": 0,
-          "target": "1-sterreichischer-platz"
+          "target": "1-ausstellung-und-information-eg"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-sterreichischer-platz",
-      "name": "Österreichischer Platz",
+      "id": "1-ausstellung-und-information-eg",
+      "name": "AUSSTELLUNG UND INFORMATION EG",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6271827187608565,
+          "pitch": 0.10186961103763714,
+          "rotation": 0,
+          "target": "2-ausstellung-1-og"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-ausstellung-1-og",
+      "name": "AUSSTELLUNG 1. OG",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.13805403597113042,
+          "pitch": -0.7934944197755378,
+          "rotation": 0,
+          "target": "3-office-atrium"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-office-atrium",
+      "name": "OFFICE ATRIUM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9023579384347613,
+          "pitch": 0.1449489379948048,
+          "rotation": 0,
+          "target": "4-office-erschlieung"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-office-erschlieung",
+      "name": "OFFICE ERSCHLIEßUNG",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3635231774537964,
+          "pitch": 0.10455267764708509,
+          "rotation": 0,
+          "target": "5-office-konferenz"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-office-konferenz",
+      "name": "OFFICE KONFERENZ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4618581725986655,
+          "pitch": 0.04988337370172857,
+          "rotation": 0,
+          "target": "6-office-break"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-office-break",
+      "name": "OFFICE BREAK",
       "levels": [
         {
           "tileSize": 256,
